6a354fd4776f7cc63c1ed67032d3fb7febacd65e
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2024-10-31 17:12:24 -0300

    conectando o back ao front

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,12 @@ const taskRoutes = require('./routes/tarefa');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: 'https://sistema-lista-de-tarefas-frontend.vercel.app/', 
+  methods: 'GET,POST,PUT,DELETE',
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json()); 
 
 app.get('/SistemaDeTarefas', (req, res) => {
