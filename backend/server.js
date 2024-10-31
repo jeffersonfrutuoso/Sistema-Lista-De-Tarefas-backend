3b0f003f8d35769e88317630766e9bdcfdeff4cb
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2024-10-31 14:25:27 -0300

    add endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); 
 
+app.get('/SistemaDeTarefas', (req, res) => {
+  res.status(200).send('OK');
+});
 
 app.use('/api/tarefas', taskRoutes);
 
